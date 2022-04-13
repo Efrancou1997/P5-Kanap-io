c595423d5cd41559de5de633b18d217255bb0f8e
Efrancou1997 <96834258+Efrancou1997@users.noreply.github.com>
2022-04-13 14:12:38 +0200

    Update confirmation.js

--- Francou_Eddy_1_code_022022/front/js/confirmation.js
+++ Francou_Eddy_1_code_022022/front/js/confirmation.js
@@ -3,3 +3,6 @@ const product_url = window.location.search;
 const urlSearchParams = new URLSearchParams(product_url);
 const orderId = urlSearchParams.get("id");
 document.getElementById("orderId").innerHTML = orderId
+
+// Effacer tout le localStorage
+localStorage.clear();
