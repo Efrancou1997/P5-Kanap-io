d375dc7fb53b52068b894e82b639468b35aeccf5
Efrancou1997 <96834258+Efrancou1997@users.noreply.github.com>
2022-04-13 14:00:01 +0200

    Update confirmation.js

--- Francou_Eddy_1_code_022022/front/js/confirmation.js
+++ Francou_Eddy_1_code_022022/front/js/confirmation.js
@@ -1,5 +1,5 @@
 // Récupération de orderId et insertion dans le HTML
-document.getElementById("orderId").innerHTML = JSON.stringify(localStorage.getItem("orderId"));
-
-// Effacer tout le localStorage
-localStorage.clear();
+const product_url = window.location.search;
+const urlSearchParams = new URLSearchParams(product_url);
+const orderId = urlSearchParams.get("id");
+document.getElementById("orderId").innerHTML = orderId
